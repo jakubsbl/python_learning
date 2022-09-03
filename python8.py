8a403a281b1d42c1f225c0004735cd9f7db12fab
jakubsbl <jakubsobol96@gmail.com>
2022-09-03 23:39:51 +0200

    i add exercises 6 to 8.

---
+++ python8.py
@@ -0,0 +1,5 @@
+# 8. Write a Python program to display the first and last colors from the following list.
+# https://www.w3resource.com/python-exercises/python-basic-exercise-8.php
+
+color_list = ["black", "green", "white", "purple", "gray" , "yellow" ]
+print(color_list[0] + " " + color_list[-1])
