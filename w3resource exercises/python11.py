78c6f56023fd1d6036ca426697bf970b048fa8bd
jakubsbl <jakubsobol96@gmail.com>
2022-09-07 13:22:15 +0200

    i add ex. 9-12

---
+++ w3resource exercises/python11.py
@@ -0,0 +1,4 @@
+# 11. Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s).
+# https://www.w3resource.com/python-exercises/python-basic-exercise-11.php
+
+print(abs.__doc__)
---
+++ w3resource exercises/python9.py
@@ -0,0 +1,8 @@
+# 9. Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+# https://www.w3resource.com/python-exercises/python-basic-exercise-9.php
+
+from re import I
+
+
+exam_st_date = (11,12,2014)
+print("The examination will start from : %i/%i/%i"%exam_st_date)
