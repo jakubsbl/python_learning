56cdf45e5cdcab736c9aa9ce3dd60a628e8cbfe9
jakubsbl <jakubsobol96@gmail.com>
2022-09-09 13:03:09 +0200

    i add tasks 13-16

---
+++ w3resource exercises/python13.py
@@ -0,0 +1,8 @@
+# 13. Write a Python program to print the following 'here document'.
+# https://www.w3resource.com/python-exercises/python-basic-exercise-13.php
+
+print(""""
+a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example""")
---
+++ w3resource exercises/python15.py
@@ -0,0 +1,8 @@
+# 15. Write a Python program to get the volume of a sphere with radius 6.
+# https://www.w3resource.com/python-exercises/python-basic-exercise-15.php
+# V = 4/3 × π × r3
+
+from math import pi
+r = 6.0
+V = 4/3*pi*r**3
+print("The volume of sphere with radius 6 is: ", V)
