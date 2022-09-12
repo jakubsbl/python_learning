9ebbcd490b0de8ad239373b74dd10e4e5d5d7651
jakubsbl <jakubsobol96@gmail.com>
2022-09-12 21:37:52 +0200

    some changes in ex. 9 and 10

--- w3resource exercises/python9.py
+++ w3resource exercises/python9.py
@@ -1,8 +1,8 @@
 # 9. Write a Python program to display the examination schedule. (extract the date from exam_st_date).
 # https://www.w3resource.com/python-exercises/python-basic-exercise-9.php
 
-from re import I
 
+exam_st_date = (11, 12, 2014)
+# print("The examination will start from : %i/%i/%i"%exam_st_date) - example from w3resource
 
-exam_st_date = (11,12,2014)
-print("The examination will start from : %i/%i/%i"%exam_st_date)
+print("The examination will start from :" + str(exam_st_date[0]) + "/" + str(exam_st_date[1]) + "/" + str(exam_st_date[2]))
